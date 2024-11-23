4e684e30cdd4b4aa0e3aef4fa8150d0885a16865
udit2303 <udit2303@gmail.com>
2024-11-24 01:14:14 +0530

    fix: cors

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ const app = express();
 const authMiddleware = require("./middleware/authMiddleware");
 const auth = require("./routes/auth");
 const event = require("./routes/event");
-const corsOptions = {
-    origin: 'http://localhost:3001', // Allow requests from the frontend (localhost:3001)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  };
-  
-  // Enable CORS for all routes with the configured options
-  app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", auth);
 app.get("/hi", (req, res) => {
